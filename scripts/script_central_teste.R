830c74d74726ffa1410a88adc2d941f6824b37fe
Bruno Grillo <bruno.grillo@ufrgs.br>
2021-11-21 17:55:51 -0300

    update development control script

--- scripts/script_central_teste.R
+++ scripts/script_central_teste.R
@@ -1,33 +1,114 @@
 library(forecast)
 library(tidyverse)
+library(lubridate)
 
 source('scripts/forecasting_methods.R')
 source('scripts/get_ts_features.R')
+source('scripts/loss_measures.R')
+source('scripts/create_rolling_ts.R')
+source('scripts/create_information_history.R')
+source('scripts/combination_methods.R')
+source('scripts/utils_functions.R')
+# FULL FUNCTION ----
+# train_meta INPUTS: (i) TIME SERIES; (i) HORIZON
+# DO ROLLING WINDOWS INSIDE FUNCTIONS
+
 # GET DATA ----
   # # GET MONTHLY DATA
 df <- Mcomp::M3
-ind_use <- lapply(df, function(x) {x$period == "MONTHLY"}) %>% unlist()
+ind_use <- lapply(df, function(x) {x$period == "MONTHLY" & length(x$x) > 90}) %>% unlist()
 df <- df[ind_use]
-
-  # # SAMPLE 50 
+  # # SAMPLE 500
 set.seed(193827)
-ind_use <- sample(seq(length(df)), size = 50, replace = FALSE)
+ind_use <- sample(seq(length(df)), size = 150, replace = FALSE)
 df <- df[ind_use]
+  # SEPARATE INTO VALIDATION AND 
+n_ahead <- 6
+df <- lapply(df, function(x) {list(train = x$x,
+                                   validation = base::subset(x$xx, start = 1, end = n_ahead) )})
+
 save(df, file = 'temp_files/ts_data.RData')
 load('temp_files/ts_data.RData')
 
-# GET FORECASTS ----
-ts_forecasts <- lapply(df, function(x) {
-  forecast_methods(y = x$x, h = 18)
-})
-save(ts_forecasts, file = 'temp_files/ts_forecasts.RData')
-load('temp_files/ts_forecasts.RData')
 
-# GET TSERIES_FEATURES ----
-ts_features <- lapply(df, function(x) {
-  get_ts_features(y_ts = x$x)
-})
-save(ts_features, file = 'temp_files/ts_features.RData')
-load('temp_files/ts_features.RData')
 
-# GET OPTIMAL WEIGHTS ----
+# GET FORECAST INFORMATIONS AT POSSIBLE TIMES (VERY TIME CONSUMING)
+library(parallel)
+nucleos <- parallel::detectCores() - 1
+
+t0 <- proc.time()
+ts_simulation <- mclapply(df, function(x) {
+  create_information_history(y_ts = x$train,           # ts 
+                             horizon = 6,              # how many steps ahead should be predicted?
+                             min_obs_lasso = 12,       # minimum number of observations to employ combination
+                             min_obs = 60,             # minimum number of observations in rolling_ts
+                             max_windows = 60)         # maximum number of rolling_ts's (must be bigger than min_obs_lasso)
+}, mc.cores = nucleos)
+proc.time() - t0
+
+save(ts_simulation, file = "ts_history_simulation.RData")
+load("ts_history_simulation.RData")
+
+# GENERATE NEW COMBINATIONS
+      # FAZER INTERAÇÃO, A CADA PERÍODO CONSIDERADO, ESTIMAR LASSO COM INFORMAÇÕES ANTERIORES
+      # FOR W IN T_PRED, FILTER(LA, T_INFO < T_PRED)
+      # NA BASE FILTRADA, APLICAR MÉTODOS DE SELEÇÃO 
+
+
+lapply(ts_simulation, function(x) {
+  
+  x 
+  
+  
+  combination_methods(new_fcts = la,
+                      y_actual = le,
+                      y_fcts = )
+  })
+
+
+
+library(tidyverse)
+library(glmnet)
+
+pred_date <- '1991-06-01'
+y_2combine <- ts_simulation$N1925$ts_forecasts %>% filter(step_ahead == 1 & t_pred < pred_date)
+y_test <- y_2combine[, -c(1:3)]
+y_actual <- actual <- matrix(y_test[, 1], ncol = 1)
+y_fcts <- forecasts <- as.matrix(y_test[, -1])
+
+new_fcts <- ts_simulation$N1925$ts_forecasts %>% filter(t_pred == pred_date)
+new_fcts <- as_mts(y = new_fcts[, -c(1:4)],
+                   x_date = new_fcts[, 3])
+
+
+# to do:
+  # combine forecasts GARCH/copula-like 
+  # estimate distribution of errors (predict variance with garch model)
+  # try finding a copula structure among the errors of predicted 
+
+
+
+
+
+
+la <-  lapply(ts_simulation, function(x) {x$ts_errors}) %>%
+  bind_rows(.id = "Variavel") %>%
+  pivot_longer(-c("Variavel", "t_pred", "step_ahead", "t_info"), 
+               names_to = "fct_candidate", 
+               values_to = "fct_error")
+
+
+ggplot(la, aes(x = fct_error, fill = as_factor(step_ahead))) +
+  geom_density(alpha = 0.3) +
+  scale_x_continuous(limits = c(0, 100)) +
+  facet_wrap(~fct_candidate) +
+  theme_bw()
+
+
+ggplot(la, aes(x = fct_error, fill = fct_candidate)) +
+  geom_density(alpha = 0.3) +
+  scale_x_continuous(limits = c(0, 100)) +
+  facet_wrap(~step_ahead) +
+  theme_bw()
+
+
